b94efaf7eb3e5dd29f515fed3fd5ed1964be0f81
d1ngn1gefe1 <alanzluo@gmail.com>
2022-03-01 03:31:49 -0800

    fix bug

--- 3rdparty/Detectron2/convert_lvis.py
+++ 3rdparty/Detectron2/convert_lvis.py
@@ -14,10 +14,10 @@ def main():
   moma = momaapi.MOMA(dir_moma, toy=True)
   lvis_indices = np.array([moma.lvis_mapper[cname] for cname in ['actor']+moma.get_cnames('object')])
 
-  w = weights['model']['roi_heads.mask_head.predictor.weight'][lvis_indices]
-  b = weights['model']['roi_heads.mask_head.predictor.bias'][lvis_indices]
-  weights['model']['roi_heads.mask_head.predictor.weight'] = w
-  weights['model']['roi_heads.mask_head.predictor.bias'] = b
+  w = weights['model']['roi_heads.box_predictor.cls_score.weight'][lvis_indices]
+  b = weights['model']['roi_heads.box_predictor.cls_score.bias'][lvis_indices]
+  weights['model']['roi_heads.box_predictor.cls_score.weight'] = w
+  weights['model']['roi_heads.box_predictor.cls_score.bias'] = b
 
   with open(os.path.join(dir_moma, 'weights/model_final_571f7c_moma.pkl'), 'wb') as f:
     pickle.dump(weights, f)
